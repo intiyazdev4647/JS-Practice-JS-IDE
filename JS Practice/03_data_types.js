13371559eb13658d5ce5c23d1253facc5b328bf6
SHAIK INTIYAZ <mrhacker94292@gmail.com>
2026-05-18 12:37:33 +0530

    Newly added changes

---
+++ JS Practice/03_data_types.js
@@ -0,0 +1,8 @@
+const student = {
+    name : "Intiyaz",
+    phone : 6304616637,
+    "marks" : 924
+}
+console.log(student["name"])
+console.log(student.name)
+console.log(student.phone)
---
+++ JS Practice/practice_set.js
@@ -0,0 +1,20 @@
+// Chapter 1 : Practice Set
+
+// 1 : Create a variable of type string and try to add a number to it.
+
+let a = "intiyaz";
+let b = 4647
+console.log(a + b)
+
+// 2 : Use typeof operator to find the data type of the string in last question
+console.log(typeof (a + b))
+
+// 3 : Create a const object in js. can you change it to hold a number later?
+const a = {
+    name: "Intiyaz",
+    marks : 924
+}
+a =
+// Ans : No, we cannot hold a number later if the variable is declared as const.
+
+//Try to add new key to the const 
